2da87165dffbfc982fe7dc0fd02b350da889d6b1
apple <apple@guozhiyingde-MacBook-Pro.local>
2019-12-06 00:12:13 +0800

    page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,46 @@
 <template>
   <div>
-    <div class="row">{{ msg }}</div>
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="form-group">
+          <h4 style="margin-bottom:5px">輸入組數</h4>
+          <input type="text" class="form-control" v-model="pairCount" />
+        </div>
+        <button type="submit" class="btn btn-primary" @click="submit">送出</button>
+      </div>
+      <div class="col-sm-12" v-if="lottoFrequencyList.length > 0">
+        <h4 style="margin-bottom:5px">出現頻率</h4>
+        <table class="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">號碼</th>
+              <th scope="col">出現次數</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(lottoItem, index) in lottoFrequencyList" :key="index">
+              <td>{{ lottoItem.lottoNumber }}</td>
+              <td>{{ lottoItem.frequency }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+      <div class="col-sm-12" v-if="lottoPairList.length > 0">
+        <h4 style="margin-bottom:5px">各組號碼清單</h4>
+        <table class="table table-hover">
+          <thead>
+            <tr>
+              <th scope="col">各組號碼</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(lottoPair, index) in lottoPairList" :key="index">
+              <td>{{ lottoPair }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -9,10 +49,30 @@ export default {
   name: "Home",
   data() {
     return {
-      msg: "Welcome to Your Vue.js App",
+      pairCount: null,
+      lottoFrequencyList: [],
+      lottoPairList: [],
     }
   },
-  methods: {},
+  methods: {
+    submit() {
+      let params = {}
+      params.pairCount = this.pairCount
+      this.$http
+        .get("/getLottoList", { params })
+        .then(response => {
+          let data = JSON.parse(response.data)
+          if (data.errorMsg) {
+            return Promise.reject(data.errorMsg)
+          }
+          this.lottoFrequencyList = data.lottoFrequencyList
+          this.lottoPairList = data.lottoPairList
+        })
+        .catch(error => {
+          console.log(String(error))
+        })
+    },
+  },
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from "vue"
+import axios from "axios"
 import "bootstrap/dist/js/bootstrap.min.js"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "iview"
@@ -6,8 +7,12 @@ import "iview/dist/styles/iview.css"
 
 import App from "./App"
 
-Vue.config.productionTip = false
+const axiosInstance = axios.create({
+  baseURL: "https://lottoexample.azurewebsites.net/api",
+})
 Vue.prototype.$IVIEW = { transfer: "" }
+Vue.prototype.$http = axiosInstance
+Vue.config.productionTip = false
 
 new Vue({
   el: "#app",
