ac8a7c575ed2714f86789fefa9f2d7676b120a9e
apple <apple@guozhiyingde-MacBook-Pro.local>
2019-12-05 21:52:50 +0800

    update package.json

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,8 @@ FROM node:10
 WORKDIR /usr/src/app
 
 COPY dist/ ./dist
-COPY index.js ./index.js
-COPY package-server.json ./package.json
+COPY server.js ./server.js
 
 RUN npm install
 EXPOSE 3000
-CMD ["node", "index.js"]
+CMD ["node", "server.js"]
--- index.js
+++ index.js
@@ -1,12 +1,75 @@
-var express = require("express")
-var app = express()
+import express from "express"
+import bodyParser from "body-parser"
+
+const app = express()
 
 app.use(express.static(__dirname + "/dist"))
+app.use(bodyParser.json())
 
-app.all("/*", (req, res) => {
-  res.sendFile(__dirname + "/dist/index.html")
+app.all("/*", (req, res, next) => {
+  if (req.path.indexOf("api") > -1) {
+    next()
+  } else {
+    res.sendFile(__dirname + "/dist/index.html")
+  }
 })
 
-var server = app.listen(3000, () => {
-  console.log(`server is running at port : ${server.address().port}`)
+const server = app.listen(3000, () => {
+  console.log(`server is running port : ${server.address().port}`)
 })
+
+app.get("/api/getLottoList", (req, res) => {
+  let pairCount = req.query.pairCount
+  if (!pairCount) {
+    res.json({ errorMsg: "Please key pairCount" })
+  }
+  let lottoFrequencyMap = initLottoFrequencyMap()
+  let lottoPairList = []
+  let lottoFrequencyList = []
+  for (let i = 0; i < pairCount; i++) {
+    let lottoPair = generateLottoPair()
+    lottoPair.forEach(lottoNumber => {
+      let frequency = lottoFrequencyMap.get(lottoNumber)
+      lottoFrequencyMap.set(lottoNumber, ++frequency)
+    })
+    lottoPairList.push(lottoPair.join(", "))
+  }
+  for (let [lottoNumber, frequency] of lottoFrequencyMap) {
+    lottoFrequencyList.push({ lottoNumber, frequency })
+  }
+  lottoPairList.sort((a, b) => {
+    return a - b
+  })
+  let response = { lottoPairList, lottoFrequencyList }
+  res.json(response)
+})
+
+function initLottoFrequencyMap() {
+  let lottoFrequencyMap = new Map()
+  for (let i = 1; i <= 49; i++) {
+    let lottoNumber = i < 10 ? `0${i}` : `${i}`
+    lottoFrequencyMap.set(lottoNumber, 0)
+  }
+  return lottoFrequencyMap
+}
+
+function generateLottoPair() {
+  let appearedArr = new Array(49)
+  let resultArr = new Array(6)
+  let index = 0
+  while (index < 6) {
+    let random = 0
+    while (random == 0) {
+      random = Math.floor(Math.random() * 50)
+    }
+    if (!appearedArr[random]) {
+      resultArr[index] = random < 10 ? `0${random}` : `${random}`
+      appearedArr[random] = true
+      index++
+    }
+  }
+  resultArr.sort((a, b) => {
+    return a - b
+  })
+  return resultArr
+}
